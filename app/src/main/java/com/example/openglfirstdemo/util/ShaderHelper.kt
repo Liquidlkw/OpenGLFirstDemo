cfeec4f1de561e1acdcbb7888ce3bba37de8f9c0
kellanlli <kellanlli@tencent.com>
2025-07-08 16:10:06 +0800

    link shader
    (cherry picked from commit bfb6ff585dd68f7d5d61b9d85eb0dde10895b159)

--- app/src/main/java/com/example/openglfirstdemo/util/ShaderHelper.kt
+++ app/src/main/java/com/example/openglfirstdemo/util/ShaderHelper.kt
@@ -27,7 +27,7 @@ object ShaderHelper {
     fun compileShader(type: Int, shaderCode: String): Int {
         //整个id是openGl对象的引用，无论什么时候想要引用这个对象，都必须使用这个id
         val shaderObjectId = GLES20.glCreateShader(type)
-        if (shaderObjectId == 0){
+        if (shaderObjectId == 0) {
             //0代表创建失败 类似java中返回null
             Log.d(TAG, "compileShader: Could not create new shader")
         }
@@ -41,9 +41,12 @@ object ShaderHelper {
         val compileStatus = IntArray(1)
         //将编译状态保存到compileStatus数组中
         GLES20.glGetShaderiv(shaderObjectId, GLES20.GL_COMPILE_STATUS, compileStatus, 0)
-        Log.d(TAG, "compileShader: Result of compiling source:\n${GLES20.glGetShaderInfoLog(shaderObjectId)}")
+        Log.d(
+            TAG,
+            "compileShader: Result of compiling source:\n${GLES20.glGetShaderInfoLog(shaderObjectId)}"
+        )
 
-        if (compileStatus[0] == 0){
+        if (compileStatus[0] == 0) {
             //编译失败 删除编译失败的shader
             GLES20.glDeleteShader(shaderObjectId)
             Log.d(TAG, "compileShader: Compilation of shader failed.")
@@ -55,7 +58,41 @@ object ShaderHelper {
     }
 
 
+    /**
+     * 链接program
+     * @param vertexShaderId 顶点shader的id
+     * @param fragmentShaderId 片段shader的id
+     * @return program的id
+     */
+    fun linkProgram(vertexShaderId: Int, fragmentShaderId: Int): Int {
+        //创建一个program对象
+        val programObjectId = GLES20.glCreateProgram()
+        if (programObjectId == 0) {
+            Log.d(TAG, "linkProgram: Could not create new program")
+            return 0
+        }
 
+        //将shader对象附加到program对象上
+        GLES20.glAttachShader(programObjectId, vertexShaderId)
+        GLES20.glAttachShader(programObjectId, fragmentShaderId)
+
+        //链接program
+        GLES20.glLinkProgram(programObjectId)
+        //获取链接状态
+        val linkStatus = IntArray(1)
+        GLES20.glGetProgramiv(programObjectId, GLES20.GL_LINK_STATUS, linkStatus, 0)
+        Log.d(TAG, "linkProgram: Result of linking program:\n${GLES20.glGetProgramInfoLog(programObjectId)}")
+
+        if (linkStatus[0] == 0) {
+            //链接失败 删除program
+            //0即失败 对象无法使用 删除对象 -这是一个固定套路！
+            GLES20.glDeleteProgram(programObjectId)
+            Log.d(TAG, "linkProgram: Linking of program failed.")
+            return 0
+        }
+
+        return programObjectId
+    }
 
 
 }
